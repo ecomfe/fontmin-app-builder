019093692449d5384bf58039c52581647aa696d9
Firede <firede@firede.us>
2015-04-09 20:21:23 +0800

    update build.js.

--- build.js
+++ build.js
@@ -1,12 +1,17 @@
+#!/usr/bin/env node
 var NwBuilder = require('node-webkit-builder');
 
 var nw = new NwBuilder({
     version: '0.12.0',
-    files: '../fontmin-app',
+    files: [
+        '../fontmin-app/**/**',
+        '!../fontmin-app/.git/**/**',
+        '!../fontmin-app/dep/webcomponentsjs/**',
+        '!../fontmin-app/node_modules/.bin/**'
+    ],
     cacheDir: './cache',
     buildDir: './build',
     macIcns: '../fontmin-app/fontmin.icns',
-    macZip: true,
     winIco: '../fontmin-app/fontmin.png',
     platforms: ['osx32', 'osx64']
     // platforms: ['win32', 'win64']
