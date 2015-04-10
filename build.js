6b0570c763b496abb4a4aa0186615febca5210ae
menglingjun <menglingjun@baidu.com>
2015-04-10 15:56:12 +0800

    windows support

--- build.js
+++ build.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 var NwBuilder = require('node-webkit-builder');
 
+var platforms = {
+    'win32': ['win32', 'win64'],
+    'darwin': ['osx32', 'osx64']
+};
+
 var nw = new NwBuilder({
     version: '0.12.0',
     files: [
@@ -13,8 +18,7 @@ var nw = new NwBuilder({
     buildDir: './build',
     macIcns: '../fontmin-app/fontmin.icns',
     winIco: '../fontmin-app/fontmin.png',
-    platforms: ['osx32', 'osx64']
-    // platforms: ['win32', 'win64']
+    platforms: platforms[process.platform]
 });
 
 //Log stuff you want
